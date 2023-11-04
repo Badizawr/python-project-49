992794eb5500d129400640d945def5d43473db06
darkwolf <darkwolf1990janary@gmail.com>
2023-11-04 10:32:30 +0300

    refactor cli

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -2,4 +2,4 @@ import prompt
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print(f"Welcome to the Brain Games!May I have your name? {name} Hello, {name}!")
+    print(f"Welcome to the Brain Games!\nMay I have your name? {name}\nHello, {name}!")
