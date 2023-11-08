177495e3e98ce92394b154c1e40c8c34b5110a89
Badizawr <vicodin-13@ya.ru>
2023-11-09 00:02:29 +0500

    work on bugs_1

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,9 @@
 import prompt
+from brain_games.scripts.brain_even import brain_even
+from brain_games.scripts.brain_calc import brain_calculator
+from brain_games.scripts.brain_gcd import brain_gcd
+from brain_games.scripts.brain_prime import brain_prime
+from brain_games.scripts.brain_progression import brain_progression
 
 
 def welcome_user():
@@ -9,5 +14,16 @@ def welcome_user():
     game_list = "\n".join(games)
     print(f"{greeting}\n{separator}\n{game_list}\n{separator}")
 
-
-welcome_user()
+    temp = input("enter the game name: ")
+    match temp:
+            case "brain-even":
+                return brain_even()
+            case "brain-calc":
+                return brain_calculator()
+            case "brain-gcd":
+                return brain_gcd()
+            case "brain-progression":
+                return brain_progression()
+            case "brain-prime":
+                return brain_prime()
+    return f"Your input {temp}"
