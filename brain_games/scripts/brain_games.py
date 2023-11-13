2fa9ff4a71924e141e7826a1687a3a3fb759c38e
darkwolf <darkwolf1990janary@gmail.com>
2023-11-13 19:43:35 +0300

    final

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
-
 import brain_games.cli
+from brain_games.scripts.brain_even import brain_even
+from brain_games.scripts.brain_calc import brain_calculator
+from brain_games.scripts.brain_gcd import brain_gcd
+from brain_games.scripts.brain_prime import brain_prime
+from brain_games.scripts.brain_progression import brain_progression
 
 
 def main():
     brain_games.cli.welcome_user()
+    temp = input("enter the game name: ")
+    match temp:
+        case "brain-even":
+            return brain_even()
+        case "brain-calc":
+            return brain_calculator()
+        case "brain-gcd":
+            return brain_gcd()
+        case "brain-progression":
+            return brain_progression()
+        case "brain-prime":
+            return brain_prime()
+    return f"Your input {temp}"
 
 
 if __name__ == '__main__':
