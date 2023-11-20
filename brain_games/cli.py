468e5a692124b10ce283aabec2101d718c143161
darkwolf <darkwolf1990janary@gmail.com>
2023-11-20 21:19:20 +0300

    refactor file cli

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,10 +2,6 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    greeting = f"Welcome to the Brain Games!\nMay I have your name? {name}\nHello, {name}!"
-    separator = "-" * 50
-    games = ["brain-even", "brain-calc", "brain-gcd", "brain-progression", "brain-prime"]
-    game_list = "\n".join(games)
-    print(f"{greeting}\n{separator}\n{game_list}\n{separator}")
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
+    print(f"\nHello, {name}!")
 
