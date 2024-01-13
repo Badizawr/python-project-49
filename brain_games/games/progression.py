11c57c04747688d4f0ec2bc1275dd18d0b0d7610
Badizawr <vicodin-13@ya.ru>
2024-01-13 22:12:00 +0500

    update9

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,22 +3,22 @@ from random import randrange
 
 TOTAL_RANGE = 10
 START_RANGE = 1
-END_RANGE = 100
+END_RANGE = 10
+RANDOM_NUMBER_RANGE = 100
 RULE = 'What number is missing in the progression?'
-TOTAL_RANGE = 10
-END_RANGE = 100
-STEP = randrange(1, 10)
-RANDOM_INDEX = randrange(1, 10)
 
 
 def generate_data():
     arithmetic_progression = []
-    start_num = randrange(END_RANGE)  # Start number of arithmetic progression
-    step = randrange(START_RANGE, TOTAL_RANGE)
+
+    start_num = randrange(RANDOM_NUMBER_RANGE)  # Start number of arithmetic progression
+    step = randrange(START_RANGE, END_RANGE)  # Step of progression
+    random_index = randrange(START_RANGE, END_RANGE)  # Random num to hide the field progression
+
     for i in range(TOTAL_RANGE):
         arithmetic_progression.append(start_num + step)
         start_num += step
-    corr_answer = str(arithmetic_progression[randrange(START_RANGE, TOTAL_RANGE)])
-    arithmetic_progression[randrange(START_RANGE, TOTAL_RANGE)] = '..'
+    corr_answer = str(arithmetic_progression[random_index])
+    arithmetic_progression[random_index] = '..'
 
     return f'{" ".join(map(str, arithmetic_progression))}', corr_answer
