bdc7b29446eae88fd875d8b15c88d48ed64ee225
DarkWolf1990 <103636861+DarkWolf1990@users.noreply.github.com>
2024-01-13 16:06:33 +0300

    Update gcd.py
    new refactoring

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,7 +7,5 @@ RULE = 'Find the greatest common divisor of given numbers.'
 
 
 def generate_data():
-    START_RANGE = 1
-    END_RANGE = 100
     a, b = randrange(START_RANGE, END_RANGE), randrange(START_RANGE, END_RANGE)
     return f'{a} {b}', str(gcd(a, b))
